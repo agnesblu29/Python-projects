c1ee4f9cdc027c1ef664e60d2b82ffb48af7bf52
Inês <114758530+agnesblu29@users.noreply.github.com>
2024-06-23 11:15:10 +0100

    Update letterCheck.py

--- letterCheck.py
+++ letterCheck.py
@@ -11,3 +11,17 @@ def letter_check(word, letter):
   
 
 print(letter_check("apple", "e"))
+
+def contains(big_string, little_string):
+  return little_string in big_string
+
+def common_letters(string_one, string_two):
+  list_lettersInCommon = []
+  for j in string_one:
+      if (j in string_two) and not(j in list_lettersInCommon):
+        list_lettersInCommon.append(j)
+  return list_lettersInCommon
+
+
+print(contains("watermelon", "melon"))
+print(common_letters("banana", "cream"))
