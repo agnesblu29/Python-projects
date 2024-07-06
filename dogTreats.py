bf6830bb345587d675df9472b52350e02acc46cb
Inês <114758530+agnesblu29@users.noreply.github.com>
2024-07-06 15:38:40 +0100

    Create dogTreats.py

---
+++ dogTreats.py
@@ -0,0 +1,23 @@
+starting_money = 100
+starting_num_items = 10
+item_price = 4
+
+# Your code here
+def buy_items(starting_money,starting_num_items,item_price):
+  num_bought = 0
+  while True :
+    if starting_num_items > 0 and num_bought <= starting_money :
+      num_bougth = starting_num_items * item_price
+    else:
+      break
+      
+  return num_bought
+
+total = buy_items(starting_money, starting_num_items, item_price)
+print("You were able to buy " + str(total) + " items.")
+  
+# For testing purposes
+total_1 = buy_items(100, 10, 4)
+print("Test 1: " + str(total_1))
+total_2 = buy_items(10, 10, 4)
+print("Test 2: " + str(total_2))
